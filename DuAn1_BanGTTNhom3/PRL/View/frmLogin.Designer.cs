79b3d28951e670cf165fec18f4d9db56dd6c4ea1
phongke004 <phongx543@gmail.com>
2023-11-25 15:20:37 +0700

    loginlainhe

--- DuAn1_BanGTTNhom3/PRL/View/frmLogin.Designer.cs
+++ DuAn1_BanGTTNhom3/PRL/View/frmLogin.Designer.cs
@@ -28,12 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmLogin";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtUser = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 19.875F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(258, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(285, 61);
+            label1.TabIndex = 0;
+            label1.Text = "Đăng nhập ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(22, 119);
+            label2.Name = "label2";
+            label2.Size = new Size(172, 36);
+            label2.TabIndex = 1;
+            label2.Text = "Tài Khoản : ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(22, 192);
+            label3.Name = "label3";
+            label3.Size = new Size(159, 36);
+            label3.TabIndex = 2;
+            label3.Text = "Mật khẩu : ";
+            // 
+            // txtUser
+            // 
+            txtUser.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtUser.Location = new Point(219, 111);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(526, 44);
+            txtUser.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txtPassword.Location = new Point(221, 189);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(524, 44);
+            txtPassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLogin.Location = new Point(201, 309);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(210, 46);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Đặng nhập";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(509, 309);
+            button2.Name = "button2";
+            button2.Size = new Size(216, 46);
+            button2.TabIndex = 6;
+            button2.Text = "Quên mật khẩu ";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUser);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmLogin";
+            Text = "frmLogin";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtUser;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private Button button2;
     }
 }
