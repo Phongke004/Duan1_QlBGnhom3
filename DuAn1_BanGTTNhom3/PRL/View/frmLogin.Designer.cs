20f8918ecc164dc2d608ce96bdd0bd9922198616
phongke004 <phongx543@gmail.com>
2023-11-25 21:39:28 +0700

    commit n1

--- DuAn1_BanGTTNhom3/PRL/View/frmLogin.Designer.cs
+++ DuAn1_BanGTTNhom3/PRL/View/frmLogin.Designer.cs
@@ -28,12 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmLogin";
+            btnLogin = new Button();
+            btnThoat = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtUser = new TextBox();
+            txtPassWord = new TextBox();
+            lbQuenMk = new Label();
+            SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            btnLogin.Font = new Font("Times New Roman", 10.875F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLogin.Location = new Point(195, 316);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(150, 46);
+            btnLogin.TabIndex = 0;
+            btnLogin.Text = "Đăng nhập";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // btnThoat
+            // 
+            btnThoat.Font = new Font("Times New Roman", 10.875F, FontStyle.Regular, GraphicsUnit.Point);
+            btnThoat.Location = new Point(452, 320);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(150, 46);
+            btnThoat.TabIndex = 1;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 19.875F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(282, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(273, 61);
+            label1.TabIndex = 2;
+            label1.Text = "Đăng nhập ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 10.875F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(59, 116);
+            label2.Name = "label2";
+            label2.Size = new Size(143, 33);
+            label2.TabIndex = 3;
+            label2.Text = "Tài khoản :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 10.875F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(67, 189);
+            label3.Name = "label3";
+            label3.Size = new Size(137, 33);
+            label3.TabIndex = 4;
+            label3.Text = "Mật khẩu :";
+            // 
+            // txtUser
+            // 
+            txtUser.Font = new Font("Times New Roman", 10.875F, FontStyle.Regular, GraphicsUnit.Point);
+            txtUser.Location = new Point(213, 116);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(489, 41);
+            txtUser.TabIndex = 5;
+            // 
+            // txtPassWord
+            // 
+            txtPassWord.Font = new Font("Times New Roman", 10.875F, FontStyle.Regular, GraphicsUnit.Point);
+            txtPassWord.Location = new Point(213, 192);
+            txtPassWord.Name = "txtPassWord";
+            txtPassWord.Size = new Size(489, 41);
+            txtPassWord.TabIndex = 6;
+            // 
+            // lbQuenMk
+            // 
+            lbQuenMk.AutoSize = true;
+            lbQuenMk.Font = new Font("Times New Roman", 7.875F, FontStyle.Regular, GraphicsUnit.Point);
+            lbQuenMk.Location = new Point(611, 396);
+            lbQuenMk.Name = "lbQuenMk";
+            lbQuenMk.Size = new Size(136, 23);
+            lbQuenMk.TabIndex = 7;
+            lbQuenMk.Text = "Quên mật khẩu";
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lbQuenMk);
+            Controls.Add(txtPassWord);
+            Controls.Add(txtUser);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnThoat);
+            Controls.Add(btnLogin);
+            Name = "frmLogin";
+            Text = "frmLogin";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnLogin;
+        private Button btnThoat;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtUser;
+        private TextBox txtPassWord;
+        private Label lbQuenMk;
     }
 }
--- DuAn1_BanGTTNhom3/PRL/View/frmLogin.cs
+++ DuAn1_BanGTTNhom3/PRL/View/frmLogin.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+using BUS.Services;
+using DAL.DomainClass;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,86 @@ namespace PRL.View
 {
     public partial class frmLogin : Form
     {
+        private NhanVienServices _service;
+        private string user, pass;
+        private bool isExitApplication = false;
         public frmLogin()
         {
+            _service = new NhanVienServices();  
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                LoginNv();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Đăng thất bại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+        private void LoginNv()
+        {
+            bool result = checkAccount();
+            if (!result) { return; }
+            try
+            {
+                frmHoaDon login = new frmHoaDon();
+                this.Hide();
+                login.ShowDialog();
+                if (login.DialogResult == DialogResult.Yes)
+                {
+                    isExitApplication = true;
+                    this.Close();
+                }
+                else
+                {
+                    this.Show();
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Đăng thất bại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private bool checkAccount()
+        {
+            bool rs = check();
+            var checkAD = _service.CheckExistsNV(user, pass);
+
+            if (!checkAD)
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không đúng!");
+                return false;
+            }
+            else
+
+            {
+                return true;
+            }
+        }
+        private bool check()
+        {
+            user = txtUser.Text.Trim();
+            pass = txtPassWord.Text.Trim();
+            if (string.IsNullOrEmpty(user) || string.IsNullOrEmpty(pass))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ tài khoản và mật khẩu!");
+                return false;
+            }
+            return true;
+        }
+        private bool checkText()
+        {
+            var user = txtUser.Text.Trim();
+            var pass = txtPassWord.Text.Trim();
+            return string.IsNullOrEmpty(user) && string.IsNullOrEmpty(pass);
+        }
+
     }
 }
