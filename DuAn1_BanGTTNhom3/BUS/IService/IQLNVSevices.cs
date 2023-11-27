4fbf3e0f34befa78ab9672fa3c40349c5c9519f8
KieuPhucs <maumebebet54@gmail.com>
2023-11-27 21:49:13 +0700

    qlnv

---
+++ DuAn1_BanGTTNhom3/BUS/IService/IQLNVSevices.cs
@@ -0,0 +1,23 @@
+﻿using DAL.DomainClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS.IService
+{
+    internal interface IQLNVSevices
+    {
+        public string Add(NhanVien nv);
+
+        public string Remove(NhanVien nv);
+        public string Update(NhanVien nv);
+
+        public List<NhanVien> GetNVs(string find);
+
+        public List<ChucVu> GetChucVus();
+
+        public List<CaLamViec> GetCaLams();
+    }
+}
---
+++ DuAn1_BanGTTNhom3/DAL/Repositories/QLNVRepos.cs
@@ -0,0 +1,57 @@
+﻿using DAL.Context;
+using DAL.DomainClass;
+using DAL.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class QLNVRepos : IQLNVRepos
+    {
+        private MyContext _dbcontext;
+
+        public QLNVRepos()
+        {
+            _dbcontext = new MyContext();
+        }
+        public bool AddNV(NhanVien nv)
+        {
+            _dbcontext.Add(nv);
+            _dbcontext.SaveChanges();
+            return true;
+        }
+
+        public List<NhanVien> GetAllNV()
+        {
+            return _dbcontext.NhanViens.ToList();
+        }
+
+        public bool RemoveNV(NhanVien nv)
+        {
+            _dbcontext.Remove(nv);
+            _dbcontext.SaveChanges();
+            return true;
+        }
+
+        public bool UpdateNV(NhanVien nv)
+        {
+            _dbcontext.Update(nv);
+            _dbcontext.SaveChanges();
+            return true;
+        }
+
+        public List<ChucVu> GetChucVus()
+        {
+            return _dbcontext.ChucVus.ToList();
+        }
+
+        public List<CaLamViec> GetCaLams()
+        {
+            return _dbcontext.CaLamViecs.ToList();
+        }
+    }
+}
